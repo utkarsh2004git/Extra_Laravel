d129883c93affb13673bb645b1885f0f9513e3ff
ukarsh2004git <utkarshshahare2004@gmail.com>
2023-12-10 14:14:34 +0530

    add table to database

---
+++ database/migrations/2023_12_10_082346_create_customer_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id('customer_id');
+            $table->string('name',60);
+            $table->string('email',60);
+            $table->string('password');
+            $table->enum('gender',['M','F','O']);
+            $table->text('address');
+            $table->date('dob')->nullable();
+            $table->boolean('status')->default(1);
+            $table->integer('points')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customer');
+    }
+};
