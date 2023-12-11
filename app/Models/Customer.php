7ca45b88606893ecf92acb791d09579c235d2755
ukarsh2004git <utkarshshahare2004@gmail.com>
2023-12-11 20:10:25 +0530

    soft delete process in model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Http\Request;
 
 class Customer extends Model
 {
 
     use HasFactory;
+    use SoftDeletes;
     protected $table="customer";
     protected $primaryKey="customer_id";
 
